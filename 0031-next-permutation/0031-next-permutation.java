9f72455cd1ea8c51922a2953ab7de153e2599d0b
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-01-04 00:56:15 +0530

    Time: 1 ms (77.86%), Space: 43.4 MB (55.08%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -12,19 +12,22 @@ class Solution {
     }
     public void nextPermutation(int[] nums) {
         if(nums.length==1 || nums==null) return;
-
+       
+        //next greater element find kro form right to left
        int i=nums.length-2;
        while(i>=0 && nums[i]>=nums[i+1]){
            i--;
        }
-
+  // jaise hi mil jaye swap kr do
        if(i>=0){
+           //required element dhundh k swap krdo
            int j=nums.length-1;
            while(j>=0 && nums[j]<=nums[i]){
                j--;
            }
            swap(nums,i,j);
        }
+        // bakki bacha usse reverse krdo
         reverse(nums,i+1,nums.length-1);
     }
 }
