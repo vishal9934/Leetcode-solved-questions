35a35d0653d54a416e78589245c58ab67232fc1a
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-01-23 14:51:56 +0530

    Time: 59 ms (6.07%), Space: 70 MB (30.57%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        
+        if(n==1) return 1;
+        Set<Integer> set=new HashSet<>();
+        Map<Integer,Integer> mp=new HashMap<>();
+
+        for(int i=0;i<trust.length;i++){
+            //add all element of 0 column bcoz cant be our ans
+            set.add(trust[i][0]);
+            
+             //add all element of 1 column and make freqency map
+          
+             mp.put(trust[i][1],mp.getOrDefault(trust[i][1],0)+1);
+        }
+        for(int key:mp.keySet()){
+            if(mp.get(key)==n-1 && set.contains(key)==false){
+                return key;
+            }
+        }
+        return -1;
+    }
+}
