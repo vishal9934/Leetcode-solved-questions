b2d574a17010d0a8ceb7e7e5de8cb8d91c82d312
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-01-29 14:52:27 +0530

    Time: 107 ms (44.54%), Space: 47 MB (94.96%) - LeetHub

---
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
@@ -0,0 +1,33 @@
+class SummaryRanges {
+TreeSet<Integer> tree;
+    public SummaryRanges() {
+        tree = new TreeSet();
+    }
+    
+    public void addNum(int value) {
+        tree.add(value);
+    }
+    
+    public int[][] getIntervals() {
+        ArrayList<int[]> interval = new ArrayList();
+        int start = tree.first();
+        int end = tree.first();
+        for(int value : tree.tailSet(tree.first()+1)){//2==>end
+            if(value-end==1){
+                end = value;
+            }else{
+                int [] arr = new int[2];
+                arr[0]=start;
+                arr[1]=end;
+                interval.add(arr);
+                start = value;
+                end = value;
+            }
+        }
+        int [] arr = new int[2];
+        arr[0]=start;
+        arr[1]=end;
+        interval.add(arr);
+        return interval.toArray(new int[interval.size()][]);
+    }
+}
