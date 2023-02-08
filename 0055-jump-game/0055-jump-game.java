6e1072f262ceb6c2204640d74a304eaed1fa9123
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-02-08 13:34:59 +0530

    Time: 1 ms (100.00%), Space: 42.7 MB (83.00%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean canJump(int[] nums) {
         int n=nums.length;
         int lastPosition=n-1;
-        for(int i=n-1;i>=0;i--){
+        for(int i=n-2;i>=0;i--){
             if(i+nums[i]>=lastPosition){
                 lastPosition=i;
             }
