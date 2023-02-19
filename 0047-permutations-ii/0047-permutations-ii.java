8c811f199ca3ad864efdd852cd10358980bb0a6c
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-02-19 17:57:31 +0530

    Time: 38 ms (24.89%), Space: 42.8 MB (89.83%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    void swap(int i,int j,int arr[]){
+        int t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Set<List<Integer>> ans=new HashSet<>();
+        rec(0,nums,ans);
+        return new ArrayList(ans);
+    }
+    void rec(int index,int arr[],Set<List<Integer>> ans){
+        
+        if(index==arr.length){
+            
+            List<Integer> ds=new ArrayList<>();
+            
+            for(int i=0;i<arr.length;i++){
+                ds.add(arr[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=index;i<arr.length;i++){
+            
+            swap(i,index,arr);
+            rec(index+1,arr,ans);
+             swap(i,index,arr);
+        }
+    }
+    
+}
