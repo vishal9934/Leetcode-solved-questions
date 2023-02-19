103d9d07bfaaa396d2a214560fae94678b952779
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-02-19 15:43:04 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (39.58%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -2,18 +2,18 @@ class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans=new ArrayList<>();
         
-        rec(1,k,n,ans,new ArrayList<>());
+        rec(1,k,n,0,ans,new ArrayList<>());
         return ans;
     }
-    void rec(int index,int k,int n,List<List<Integer>> ans,List<Integer> ds){
-        if(ds.size()==k && n==0){
+    void rec(int index,int k,int n,int sum,List<List<Integer>> ans,List<Integer> ds){
+        if(sum==n && k==0){
             ans.add(new ArrayList<>(ds));
             return;
         }
         
         for(int i=index;i<=9;i++){
             ds.add(i);
-            rec(i+1,k,n-i,ans,ds);
+            rec(i+1,k-1,n,sum+i,ans,ds);
             ds.remove(ds.size()-1);
         }
     }
