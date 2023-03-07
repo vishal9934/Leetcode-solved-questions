a326d58e53fd56121b60489993735d344ce940e9
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-03-07 21:18:24 +0530

    Time: 203 ms (79.96%), Space: 55.7 MB (89.54%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -1,70 +1,70 @@
-// class Solution {
-//     boolean check(long mid,int[] time,int totalTrips){
-//         long trip=0;
-//         for(int x:time){
-//             trip+=totalTrips/x;
-//         }
-//         if(trip>=mid){
-//             return true;
-//         }
-//         return false;
-//     }
-//     public long minimumTime(int[] time, int totalTrips) {
-//         long low=Long.MAX_VALUE;
-//         long min=Long.MAX_VALUE;
-        
-//         for(int x:time){
-//             low =Math.min(low,x);
-//             min=Math.min(min,x);
-            
-//         }
-//         long high=min*totalTrips;
-        
-        
-//         while(low<=high){
-//             long mid=low+(high-low)/2;
-            
-//             if(check(mid,time,totalTrips)){
-//                 high=mid-1;
-//             }
-//             else{
-//                 low=mid+1;
-//             }
-//         }
-//         return low;
-//     }
-// }
-
-
-
-
 class Solution {
+    boolean check(long mid,int[] time,int totalTrips){
+        long trip=0;
+        for(int x:time){
+            trip+=mid/x;
+        }
+        if(trip>=totalTrips){
+            return true;
+        }
+        return false;
+    }
     public long minimumTime(int[] time, int totalTrips) {
         long low=Long.MAX_VALUE;
-        long high=0;
         long min=Long.MAX_VALUE;
-        for(int it:time){
-            low=Math.min(low,it);
-            min=Math.min(min,it);
+        
+        for(int x:time){
+            low =Math.min(low,x);
+            min=Math.min(min,x);
+            
         }
-        high=totalTrips*min;
-        while(low<high){
+        long high=min*totalTrips;
+        
+        
+        while(low<=high){
             long mid=low+(high-low)/2;
-            if(blackbox(mid,totalTrips,time)){
-                high=mid;
+            
+            if(check(mid,time,totalTrips)){
+                high=mid-1;
             }
-            else
+            else{
                 low=mid+1;
+            }
         }
         return low;
     }
-    public boolean blackbox(long isvalidtime,int totalTrips,int[] time){
-        long trips=0;
-        for(int it:time){
-            trips+=isvalidtime/it;
-        }
-        if(trips>=totalTrips)
-            return true;
-        return false;
-    }
 }
+
+
+
+
+// class Solution {
+//     public long minimumTime(int[] time, int totalTrips) {
+//         long low=Long.MAX_VALUE;
+//         long high=0;
+//         long min=Long.MAX_VALUE;
+//         for(int it:time){
+//             low=Math.min(low,it);
+//             min=Math.min(min,it);
+//         }
+//         high=totalTrips*min;
+//         while(low<high){
+//             long mid=low+(high-low)/2;
+//             if(blackbox(mid,totalTrips,time)){
+//                 high=mid;
+//             }
+//             else
+//                 low=mid+1;
+//         }
+//         return low;
+//     }
+//     public boolean blackbox(long isvalidtime,int totalTrips,int[] time){
+//         long trips=0;
+//         for(int it:time){
+//             trips+=isvalidtime/it;
+//         }
+//         if(trips>=totalTrips)
+//             return true;
+//         return false;
+//     }
+// }
