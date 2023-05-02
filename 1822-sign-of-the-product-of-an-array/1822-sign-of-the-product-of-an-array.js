8cf55ed2e2001c61a700a700b4469d7de837ce36
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-05-02 10:48:29 +0530

    Time: 53 ms (90.31%), Space: 42 MB (93.25%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let product=1;
+    for(let i=0;i<nums.length;i++){
+       if(nums[i]<0){
+           product*=-1;
+       }
+        if(nums[i]==0){
+            return 0;
+        }
+    }
+    return product;
+   
+   
+};
