938672443e53692199e85bab4aad445278a1cdcd
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-05-25 22:54:14 +0530

    Time: 53 ms (94.26%), Space: 44.4 MB (62.13%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+var chunk = function(arr, size) {
+    let newArr=[];
+    for(let i=0;i<arr.length;i+=size){
+        newArr.push(arr.slice(i,i+size))
+    }
+    return newArr
+};
