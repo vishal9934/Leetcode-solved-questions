1c472297fe3651328c4ee8a35510c8b69b4d0d95
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2024-02-13 22:03:44 +0530

    Time: 2 ms (71.53%), Space: 44.7 MB (84.10%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2108-find-first-palindromic-string-in-the-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        
+        for(String word: words){
+            if(isPalindrome(word)){
+                return word;
+            }
+        }
+        return "";
+    }
+
+
+    public boolean isPalindrome(String s){
+        int left=0;
+        int right= s.length()-1;
+                  while(left<right){
+        if(s.charAt(left++)!=s.charAt(right--)){
+            return false;
+        }
+                  }
+        return true;
+    }
+}
